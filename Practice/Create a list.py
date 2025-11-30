3381034f182253673c11529e25e1c31926b166f9
sina karimi <sinak5081@gmail.com>
2025-11-30 12:09:34 -0800

    Ways to make a list

---
+++ Practice/Create a list.py
@@ -0,0 +1,12 @@
+#List creation methods
+#The first method
+#Create an empty list
+lst = list()
+#___________
+lst = []
+#Create an empty list
+#_________________________________
+lst = [12,15.5,"computer","physics"]
+#Creating a list with four elements
+#___________________________________________
+print(lst)
