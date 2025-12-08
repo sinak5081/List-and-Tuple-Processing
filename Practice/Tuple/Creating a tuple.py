a020c5253383e26eec63927d640e8a32d0f020ee
sina karimi <sinak5081@gmail.com>
2025-12-08 12:41:13 -0800

    Methods for constructing tuples

---
+++ Practice/Tuple/Creating a tuple.py
@@ -0,0 +1,27 @@
+#first method to create tuple
+a = tuple()
+#________________________________________
+#second method
+b = ()
+#______________________________
+#third method
+c = tuple([5])
+print(type(c))
+#show you class t
+#_______________________
+#look output
+d = (5)
+print(type(d))
+# show you class int
+#_____________________________
+#look
+f = (5,)
+print(type(f))
+#show class tuple
+#To create a tuple with one element without using the tuple class constructor, we must put a comma after the element.
+g = (10,15,20)
+print(type(g))
+#_____________________
+#With the tuple constructor class method
+h = tuple([10,15,20])
+print(type(h))
