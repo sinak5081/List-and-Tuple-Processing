559e52f6f3b2f72d15a53aa832e1c4a0245ded6e
sina karimi <sinak5081@gmail.com>
2025-12-08 11:20:51 -0800

    Creating lists with different line lengths

---
+++ Practice/Different line lengths.py
@@ -0,0 +1,13 @@
+term  = 3
+n1,n2,n3 = 5,3,6
+grades = ([0] * term)
+grades[0] = ([0] * n1)
+grades[1] = ([0] * n2)
+grades[2] = ([0] * n3)
+#Enter values in list:
+grades[0] = [1,2,3,4,5]
+grades[1] = [1,2,3]
+grades[2] = [1,2,3,4,5,6]
+print(grades[0])
+print(grades[1])
+print(grades[2])
